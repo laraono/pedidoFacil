57f16e764070a7999ade40fff12d4ae0a2f145db
gabriela <gabriela.gandine@gmail.com>
2026-02-21 14:25:24 -0300

    feat: add pedidos to a comanda

--- web/src/views/app/Menu.vue
+++ web/src/views/app/Menu.vue
@@ -2,12 +2,27 @@
 import localStorageService from '@/services/localStorageService';
 import { ref, computed, onMounted } from 'vue';
 import { useMenuStore } from '@/stores/productsManagement';
+import { useComandaStore } from '@/stores/comandaManagement';
 
 const imageUrl = ref('');
 
 const produtos = ref([]);
 
+const selectedProduto = ref({})
+
+const amount = ref([])
+
+const isOpen = ref(false)
+
+const hasPedido = ref(false)
+
+const pedidoEnded = ref(false)
+
+const pedido = ref([])
+
 const menuStore = useMenuStore();
+
+const comandaStore = useComandaStore();
 
 const backgroundStyle = computed(() => {
     return {
@@ -36,6 +51,74 @@ const selectCategory = (id) => {
 
 }
 
+const saveAmount = (change, product, size) => {
+    const key = `${product.name}-${size.name}`
+  
+    if (!amount.value[key]) {
+        amount.value[key] = 0
+    }
+
+    amount.value[key] += change
+    
+    if (amount.value[key] < 0) {
+        amount.value[key] = 0
+        return
+    }
+    
+    const existingItemIndex = pedido.value.findIndex(
+        item => item.produto === product.name && item.size === size.name
+    )
+
+    if(amount.value[key] !== 0) {
+        if (existingItemIndex !== -1) {
+            pedido.value[existingItemIndex].amount = amount.value[key]
+            pedido.value[existingItemIndex].price = amount.value[key] * size.price
+        } else {
+            pedido.value.push({
+                produto: product.name, 
+                size: size.name, 
+                amount: amount.value[key],
+                price: amount.value[key] * size.price
+            })
+
+        }
+    }
+}
+
+const cancelPedido = () => {
+    isOpen.value = false
+
+    pedido.value = []
+    amount.value = []
+}
+
+const salvarPedido = () => {    
+    hasPedido.value = true
+    isOpen.value = false
+    amount.value = []
+}
+
+const calculateTotal = () => {
+    return pedido.value.reduce((sum, item) => sum + (item.price || 0), 0)
+}
+
+
+const addComanda = (order) => {
+    comandaStore.addComanda(order, calculateTotal())
+
+    pedido.value = []
+
+    hasPedido.value =false
+}
+
+const updateComanda = (id, order) => {
+    comandaStore.updateComanda(id, order, calculateTotal())
+
+    pedido.value = []
+
+    hasPedido.value = false    
+}
+
 </script>
 
 <template>
@@ -60,7 +143,7 @@ const selectCategory = (id) => {
             class="w-60 h-lvh transform transition-all duration-300 overflow-hidden"
             :style="{ background: localStorageService.getCategoryColors() }"
         >
-            <div class="flex flex-col  items-center py-4 h-screen overflow-auto">
+            <div class="flex flex-col  items-center py-4 h-screen overflow-auto p-4">
                 <div v-for="category in menuStore.categories" :key="category.id" >
                     <button @click="selectCategory(category.id)" class="image-button flex flex-col items-center justify-center w-full h-full p-2">
                         <img :src="category.image" class="button-icon w-18 h-18 object-contain max-w-full max-h-full"/>
@@ -76,17 +159,119 @@ const selectCategory = (id) => {
         >
             <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-3 p-4">
                 <div v-for="produto in produtos" :key="produto.id" >
-                    <button @click="handleClick" class="image-button flex flex-col items-center justify-center w-full h-full p-4 " :style="{background: localStorageService.getButtonColors()}">
+                    <button 
+                        @click="isOpen = true, selectedProduto = produto" class="image-button flex flex-col items-center justify-center w-full h-full p-4 " 
+                        :style="{background: localStorageService.getButtonColors()}"
+                    >
                             <img :src="produto.image" class="button-icon object-contain max-w-full max-h-full"/>
-                            <span>{{ produto.name }}</span>
-                            <span>{{ produto.description }}</span>
-                            <span>{{ produto.sizes[0].price }}</span>
+                            <div class="flex justify-between w-full mb-1">
+                                <span class="font-medium">{{ produto.name }}</span>
+                                <span class="font-semibold"> {{ 'R$ ' + produto.sizes[0].price }}</span>
+                            </div>
+                            <span class="text-sm ">{{ produto.description }}</span>
                     </button>
                 </div>            
             </div>
         
         </div>
 
+        <div v-if="hasPedido" class="fixed bottom-0 w-full bg-black p-4">
+            <div class="flex justify-between w-full mb-1">
+                <span class="font-bold text-white"> Produto </span>
+                <span class="font-bold text-white"> Tamanho </span>
+                <span class="font-bold text-white"> Quantidade </span>
+                <span class="font-bold text-white"> Preço </span>
+
+            </div>
+            <div class="flex justify-between w-full mb-1" v-for="item in pedido">
+                <span class="font-medium text-white">{{ item.produto }}</span>
+                <span class="font-medium text-white">{{ item.size }}</span>
+                <span class="font-medium text-white">{{ item.amount }}</span>
+                <span class="font-medium text-white"> {{'R$' + item.price }}</span>
+            </div>
+
+            <div class="flex justify-between items-center pt-2 border-t border-gray-700">
+                <div class="text-white">
+                    <span class="font-bold">Total: </span>
+                    <span class="text-xl font-bold text-green-400">
+                        R$ {{ calculateTotal().toFixed(2) }}
+                    </span>
+                </div>
+                
+                    <button 
+                        @click="pedidoEnded = true"
+                        class="font-medium text-white px-6 py-2 rounded transition-colors"
+                        :style="{background: localStorageService.getButtonColors()}"
+                    > 
+                        Finalizar Pedido
+                    </button>
+            </div>
+
+        </div>
+
+        <Teleport to="body">
+            <div v-if="isOpen" class="fixed inset-0 bg-black/50 flex items-center justify-center p-4">
+                <div class="bg-white p-4 rounded-lg max-w-2xl w-full">
+                    <h2 class="text-black font-bold">{{ selectedProduto.name }}</h2>
+                    <div class="flex justify-between w-full mb-1" v-for="(size, index) in selectedProduto.sizes">
+                        <span class="font-medium text-black">{{ size.name }}</span>
+                        <span class="font-medium text-black"> {{ 'R$ ' + size.price }}</span>
+                        <div class="flex justify-between">
+                            <button @click="saveAmount(-1, selectedProduto, size)" class="font-medium text-black"> - </button>
+                            <span class="font-medium text-black"> {{ amount[selectedProduto.name + '-' + size.name] || 0   }}</span>
+                            <button @click="saveAmount(1, selectedProduto, size)" class="font-medium text-black"> + </button>
+                        </div>
+                        
+                    </div>
+                    <div class="flex justify-between w-full mb-1">
+                        <button @click="cancelPedido" :style="{background: localStorageService.getButtonColors()}" class="text-black p-2">Cancelar</button>
+                        <button @click="salvarPedido" :style="{background: localStorageService.getButtonColors()}" class="text-black p-2">Salvar Pedido</button>
+                    </div>
+                </div>
+            </div>
+        </Teleport>
+
+
+        <Teleport to="body">
+            <div v-if="pedidoEnded" class="fixed inset-0 flex items-center justify-center p-4">
+                <div class="bg-white p-4 rounded-lg max-w-4xl w-full ">
+                    <div class="grid grid-cols-2 md:grid-cols-3 gap-4 mb-4">
+                        <div class="flex item-start justify-between mb-1 bg-green-600 p-4 rounded-lg" v-for="comanda in comandaStore.comandas">
+                            <button @click="updateComanda(comanda.id, pedido)" class="w-full">
+                                <div class="bg-green-500 w-full p-2 rounded-t-lg"> <!-- Adicionado w-full e padding -->
+                                    <h2 class="text-white font-bold text-lg">{{ 'Comanda ' + comanda.id }}</h2>
+                                </div>
+                                <div class="flex flex-col w-full p-2" v-for="order in comanda.orders"> <!-- Adicionado padding -->
+                                    <div class="flex justify-between w-full">
+                                        <span class="font-bold text-white">{{ order.amount + 'x' }}</span>
+                                        <span class="font-bold text-white">{{ order.produto }}</span>
+                                    </div>
+                                </div>          
+                                <span class="font-medium text-white text-right block w-full p-2"> <!-- Adicionado block, w-full e padding -->
+                                    {{ 'R$ ' + comanda.totalPrice }}
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="flex justify-between w-full mb-1">
+                        <button 
+                            @click="addComanda(pedido)"
+                            :style="{background: localStorageService.getButtonColors()}" class="text-black p-2"
+                        >
+                            Criar Comanda
+                        </button>
+
+                        <button 
+                            @click="pedidoEnded = false" 
+                            :style="{background: localStorageService.getButtonColors()}" class="text-black p-2"
+                        >
+                            Fechar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Teleport>
 
     
     </div>
