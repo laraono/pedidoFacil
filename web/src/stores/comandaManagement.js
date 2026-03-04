4c446e1c2e8e3e9cbae7542e56f2556eafadefd2
gabi <gabriela.gandine@gmail.com>
2026-03-04 07:15:11 -0300

    refactor: change comandas ids value

--- web/src/stores/comandaManagement.js
+++ web/src/stores/comandaManagement.js
@@ -4,28 +4,19 @@ import { ref } from 'vue';
 export const useComandaStore = defineStore('comanda', () => {
 
     const comandas = ref([])
-    const orders = ref([])
 
     const addComanda = (order) => {
-        comandas.value.push({ orders: [order], id: Date.now(), total: order.price });
+        comandas.value.push({ orders: [order], id: comandas.value.length, total: order.price });
     };
 
     const updateComanda = (id, order, totalPrice) => {
-        console.log('antes do update: ', comandas.value)
-
-        console.log('id - ', id)
-        console.log('totalPrice - ', totalPrice)
-        console.log('order - ', order)
-
         const index = comandas.value.findIndex(c => c.id === id);
-        console.log('index - ', index)
 
         if (index !== -1) {
             comandas.value[index].orders.push(order)
             comandas.value[index].total += totalPrice
         }
 
-        console.log('depois do update: ', comandas.value)
     };
 
     const removeComanda = (id) => {
