56845a6e187df9f308596b53f1f9efac07865acf
Lenovo <gabriela.gandine@gmail.com>
2026-03-14 20:30:16 -0300

    feat: order and comanda status

---
+++ api/src/enum/index.ts
@@ -0,0 +1,1 @@
+export {ComandaStatus, PedidoStatus} from './status'
---
+++ api/src/enum/status.ts
@@ -0,0 +1,12 @@
+export enum PedidoStatus {
+    AGUARDANDO_PREPARO = 'Aguardando_Preparo',
+    EM_PREPARO = 'Em_Preparo',
+    PRONTO = 'Pronto',
+    FINALIZADO = 'Finalizado',
+    CANCELADO = 'Cancelado'
+}
+
+export enum ComandaStatus {
+    ABERTA = 'Aberta',
+    FECHADA = 'Fechada'
+}
