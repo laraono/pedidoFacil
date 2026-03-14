32dda99c2b312f285c5157dd7090c9b92ab4401a
Lenovo <gabriela.gandine@gmail.com>
2026-03-14 20:29:13 -0300

    refactor: change tab from 2 to 4

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,24 +9,24 @@ app.use(express.json());
 
 // Configuração da conexão com o banco
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'pedido_facil'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'pedido_facil'
 });
 
 // Rota de teste
 app.get('/', async (req, res) => {
-  try {
-    // Testando conexão com o banco
-    const [rows] = await db.query('SELECT 1 + 1 AS result');
-    res.json({ message: 'API Pedido Fácil Online!', db_connection: 'OK', data: rows });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao conectar no banco', details: error });
-  }
+    try {
+        // Testando conexão com o banco
+        const [rows] = await db.query('SELECT 1 + 1 AS result');
+        res.json({ message: 'API Pedido Fácil Online!', db_connection: 'OK', data: rows });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao conectar no banco', details: error });
+    }
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server rodando em http://localhost:${PORT}`);
+    console.log(`🚀 Server rodando em http://localhost:${PORT}`);
 });
