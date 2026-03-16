f62c9750c08ee4c35e7b4f94ed9453b657408868
Lenovo <gabriela.gandine@gmail.com>
2026-03-16 16:13:58 -0300

    fix index

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,38 +3,23 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import { categoryRouter, comandaRouter, orderRouter, productRouter } from './router';
+import { AppDataSource } from './database';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+AppDataSource.initialize().then(async () => {
+    app.use('/api/v1', categoryRouter)
+    app.use('/api/v1', comandaRouter)
+    app.use('/api/v1', orderRouter)
+    app.use('/api/v1', productRouter)
 
-// Configuração da conexão com o banco
-const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'pedido_facil'
-});
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`🚀 Server rodando em http://localhost:${PORT}`);
+    });
+})
 
-// Rota de teste
-app.get('/', async (req, res) => {
-    try {
-        // Testando conexão com o banco
-        const [rows] = await db.query('SELECT 1 + 1 AS result');
-        res.json({ message: 'API Pedido Fácil Online!', db_connection: 'OK', data: rows });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao conectar no banco', details: error });
-    }
-});
 
-app.use('api/v1', categoryRouter)
-app.use('/api/v1', comandaRouter)
-app.use('/api/v1', orderRouter)
-app.use('/api/v1', productRouter)
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server rodando em http://localhost:${PORT}`);
-});
