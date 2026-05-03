78d4c0448b674c734ac673cd8d94860ebb025a34
gabi-gandine <gabriela.gandine@gmail.com>
2026-05-03 15:00:24 -0300

    feat: add new subscriptions routes

--- backend/src/router/SubscriptionRouter.ts
+++ backend/src/router/SubscriptionRouter.ts
@@ -2,19 +2,20 @@
 import express from 'express';
 import authenticate from '../middleware/authenticate';
 import { subscriptionController } from '../controller';
-import { validateCreateSubscription } from '../validator';
+import { validateCreateSubscription, validateListSubscriptions, validateRestoreSubscription } from '../validator';
 
 const subscriptionRouter = express.Router();
 
-subscriptionRouter.use(authenticate);
-
-subscriptionRouter.get('/subscriptions/', subscriptionController.listSubscriptions);
-subscriptionRouter.get('/plans/:planId/subscriptions/', subscriptionController.listSubscriptionsByPlan);
-subscriptionRouter.get('/plans/:planId/subscriptions/:subscriptionId', subscriptionController.getSubscription);
-subscriptionRouter.post('/process-order', validateCreateSubscription, subscriptionController.processCardInfo);
+subscriptionRouter.get('/subscriptions/', authenticate, subscriptionController.listSubscriptions);
+subscriptionRouter.get('/plans/:planId/subscriptions/', authenticate, subscriptionController.listSubscriptionsByPlan);
+subscriptionRouter.get('/plans/:planId/subscriptions/:subscriptionId', authenticate, subscriptionController.getSubscription);
+subscriptionRouter.post('/process-order', authenticate, validateCreateSubscription, subscriptionController.processCardInfo);
+subscriptionRouter.post('/process-order/:subscriptionId', authenticate, validateRestoreSubscription, subscriptionController.restoreSubscription);
 subscriptionRouter.delete('/plans/:planId/subscriptions/:subscriptionId', subscriptionController.deleteSubcription);
 subscriptionRouter.put('/plans/:planId/subscriptions/:subscriptionId', subscriptionController.updateSubscriptionPrice);
-subscriptionRouter.post('/plans/:planId/subscriptions/:subscriptionId/cancel', subscriptionController.cancelSubcription);
+subscriptionRouter.post('/plans/:planId/subscriptions/:subscriptionId/cancel', authenticate, subscriptionController.cancelSubcription);
 subscriptionRouter.get('/process-oauth', subscriptionController.processCardInfo)
+subscriptionRouter.get('/subscriptions/establishment', authenticate, validateListSubscriptions, subscriptionController.getEstablishmentSubscription)
+subscriptionRouter.get('/subscriptions/history', authenticate, validateListSubscriptions, subscriptionController.getEstablishmentHistory)
 
 export { subscriptionRouter };
